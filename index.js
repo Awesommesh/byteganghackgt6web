42ca319241b478cd52d8aa0375b059b6e427e62f
Animesh Agrawal <animesh.a.777@gmail.com>
2019-10-26 14:54:37 -0400

    in progress

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const fs = require('fs');
 const readline = require('readline');
-//const {google} = require('googleapis');
-/*
+const {google} = require('googleapis');
+const request = require('request'); //For Weather API
+const http = require('http');
+const url = require('url');
+const opn = require('open');
+const destroyer = require('server-destroy');
+const path = require('path');
+var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+var argv = require('optimist').argv;
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
+app.use('/js', express.static(__dirname + '/js'));
+app.use(morgan('dev')); 										// log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 									// parse application/json
+app.use(methodOverride());
+
+app.get('/', function(req, res) {
+	res.sendfile('main.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+app.listen(8080, argv.fe_ip);
+console.log("App listening on port 8080");
+
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
@@ -22,7 +48,7 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-/*
+
 function authorize(credentials, callback) {
 	const {client_secret, client_id, redirect_uris} = credentials.installed;
 	const oAuth2Client = new google.auth.OAuth2(
@@ -42,7 +68,7 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-/*
+
 function getAccessToken(oAuth2Client, callback) {
 	const authUrl = oAuth2Client.generateAuthUrl({
 		access_type: 'offline',
@@ -72,7 +98,7 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-/*
+
 function listEvents(auth) {
 	const calendar = google.calendar({version: 'v3', auth});
 	calendar.events.list({
@@ -94,14 +120,13 @@ function listEvents(auth) {
 			console.log('No upcoming events found.');
 		}
 	});
-}*/
+}
 
-const request = require('request'); //For Weather API
 let apiKey = "46f2b22404f803839b2772708544f597";
 let area = "America";
 let city = "Atlanta";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-request(url, function (err, response, body) {
+let urli = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+request(urli, function (err, response, body) {
 	if(err){
 		console.log('error:', error);
 	} else {
@@ -109,15 +134,15 @@ request(url, function (err, response, body) {
 	}
 });
 city = "New_York";
-url = `http://worldtimeapi.org/api/timezone/${area}/${city}.txt`;
-request(url, function (err, response, body) {
+urli = `http://worldtimeapi.org/api/timezone/${area}/${city}.txt`;
+request(urli, function (err, response, body) {
 	if(err){
 		console.log('error:', error);
 	} else {
 		console.log('body:', body);
 	}
 });
-
+/*
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
@@ -134,4 +159,4 @@ app.get('/', function(req, res) {
 });
 
 app.listen(8080, argv.fe_ip);
-console.log("App listening on port 8080");
+console.log("App listening on port 8080");*/
