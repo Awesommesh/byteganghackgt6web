a1062f30ea121d8668f313e3de25ed626c5353d6
Animesh Agrawal <animesh.a.777@gmail.com>
2019-10-26 13:47:04 -0400

    web server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+var morgan = require('morgan'); 			// log requests to the console (express4)
+var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var argv = require('optimist').argv;
+
+/* communicate with cloud api
+ */
